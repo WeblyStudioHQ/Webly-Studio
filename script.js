cfb5b8d27b15de4e4a3b4832e2b3de61e703d4c3
Webly Studio <mariyainamdar84@gmail.com>
2026-01-30 17:20:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+﻿// Smooth hover glow effect on cards/buttons later
+document.querySelectorAll(".btn").forEach(btn => {
+  btn.addEventListener("mouseenter", () => {
+    btn.style.boxShadow = "0 0 15px #4dd2ff";
+  });
+  btn.addEventListener("mouseleave", () => {
+    btn.style.boxShadow = "none";
+  });
+});
